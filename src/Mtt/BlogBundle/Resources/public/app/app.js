d2c0f418120a15630de59b42517358b32bd43bb3
morontt <morontt@gmail.com>
2014-11-17 00:38:15 +0200

    base layout

--- src/Mtt/BlogBundle/Resources/public/app/app.js
+++ src/Mtt/BlogBundle/Resources/public/app/app.js
@@ -2,7 +2,8 @@
  * Created by morontt on 16.11.14.
  */
 window.MttBlog = Ember.Application.create({
-    LOG_TRANSITIONS: true
+    rootElement: '#main-application'
+    , LOG_TRANSITIONS: true
     , LOG_VIEW_LOOKUPS: true
 });
 
