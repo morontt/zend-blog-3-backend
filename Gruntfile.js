1ba7ec4bf5f940628a208b08826fe51b9556699f
morontt <morontt@gmail.com>
2015-02-20 16:28:57 +0200

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        emberTemplates: {
+            compile: {
+                options: {
+                    templateName: function(sourceFile) {
+                        return sourceFile.replace(/src\/Mtt\/BlogBundle\/Resources\/public\/app\/templates\//, '');
+                    },
+                    templateCompilerPath: 'src/Mtt/BlogBundle/Resources/public/components/ember/ember-template-compiler.js',
+                    handlebarsPath: 'src/Mtt/BlogBundle/Resources/public/components/handlebars/handlebars.js'
+                },
+                files: {
+                    "src/Mtt/BlogBundle/Resources/public/app/templates.js": "src/Mtt/BlogBundle/Resources/public/app/templates/*.hbs"
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-ember-templates');
+    grunt.registerTask('default', ['emberTemplates']);
+};
