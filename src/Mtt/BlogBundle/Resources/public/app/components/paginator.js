c3fa5efaf08d2bec936bbc2d01ee29dc4fbe0c06
morontt <morontt@gmail.com>
2015-03-02 23:27:50 +0200

    simple paginator complete

--- src/Mtt/BlogBundle/Resources/public/app/components/paginator.js
+++ src/Mtt/BlogBundle/Resources/public/app/components/paginator.js
@@ -4,4 +4,18 @@
  * Time: 1:16
  */
 
-MttBlog.PaginatorListComponent = Ember.Component.extend({});
+MttBlog.PaginatorListComponent = Ember.Component.extend({
+    pageLinks: function () {
+        var meta = this.get('meta');
+        var i, links = [];
+
+        for (i = 1; i <= meta.last; i++) {
+            links.push({
+                page: i,
+                active: (meta.current == i)
+            });
+        }
+
+        return links;
+    }.property('meta')
+});
