e9fede382bf2a371d9d745c72e1687d895993317
morontt <morontt@gmail.com>
2015-05-02 00:43:55 +0300

    add assetic filters

--- src/Mtt/BlogBundle/Resources/public/app/app.js
+++ src/Mtt/BlogBundle/Resources/public/app/app.js
@@ -2,11 +2,16 @@
  * Created by morontt on 16.11.14.
  */
 
-window.MttBlog = Ember.Application.create({
+var application_options = {
     rootElement: '#main-application'
-    , LOG_TRANSITIONS: true
-    , LOG_VIEW_LOOKUPS: true
-});
+};
+
+if (app_parameters.environment === 'dev') {
+    application_options.LOG_TRANSITIONS = true;
+    application_options.LOG_VIEW_LOOKUPS = true;
+}
+
+window.MttBlog = Ember.Application.create(application_options);
 
 MttBlog.ApplicationAdapter = DS.ActiveModelAdapter.extend({
     namespace: app_parameters.api_url
