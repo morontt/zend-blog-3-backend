126f3183e70529922f4402e64b981b1e11723538
morontt <morontt@gmail.com>
2015-06-17 22:00:14 +0300

    save access token #12

--- src/Mtt/BlogBundle/Command/DropboxAuthCommand.php
+++ src/Mtt/BlogBundle/Command/DropboxAuthCommand.php
@@ -9,6 +9,7 @@
 namespace Mtt\BlogBundle\Command;
 
 use Dropbox;
+use Mtt\BlogBundle\Entity\SystemParameters;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -42,7 +43,7 @@ class DropboxAuthCommand extends ContainerAwareCommand
         $authorizeUrl = $webAuth->start();
 
         $output->writeln(sprintf("\n1. Go to: %s", $authorizeUrl));
-        $output->writeln('2. Click "Allow" (you might have to log in first).');
+        $output->writeln("2. Click <comment>\"Allow\"</comment> (you might have to log in first).");
         $output->writeln("3. Copy the authorization code.\n");
 
         $dialog = $this->getHelper('question');
@@ -62,7 +63,36 @@ class DropboxAuthCommand extends ContainerAwareCommand
         list($accessToken, $userId) = $webAuth->finish($authCode);
 
         $output->writeln("\nAuthorization complete.");
-        $output->writeln(sprintf('User ID:      %s', $userId));
-        $output->writeln(sprintf('Access Token: %s', $accessToken));
+        $output->writeln(sprintf("User ID: <comment>%s</comment>", $userId));
+        $output->writeln(sprintf("Access Token: <comment>%s</comment>", $accessToken));
+
+        $this->saveAccessToken($userId, $accessToken);
+    }
+
+    /**
+     * @param $userId
+     * @param $accessToken
+     */
+    protected function saveAccessToken($userId, $accessToken)
+    {
+        $em = $this->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $sp = $em->getRepository('MttBlogBundle:SystemParameters')
+            ->findOneByOptionKey(SystemParameters::DROPBOX_TOKEN);
+
+        if (!$sp) {
+            $sp = new SystemParameters();
+            $sp->setOptionKey(SystemParameters::DROPBOX_TOKEN);
+            $em->persist($sp);
+        }
+
+        $sp->setValue(serialize([
+            'user_id' => $userId,
+            'access_token' => $accessToken,
+        ]));
+
+        $em->flush();
     }
 }
