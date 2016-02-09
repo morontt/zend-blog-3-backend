f2a94e76689aaee4239bad62c9924516960131a2
morontt <morontt@gmail.com>
2016-02-10 00:12:17 +0200

    move templates

--- spa/app/app.js
+++ spa/app/app.js
@@ -10,7 +10,8 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
     modulePrefix: config.modulePrefix,
     podModulePrefix: config.podModulePrefix,
-    Resolver
+    Resolver,
+    rootElement: '#main-application'
 });
 
 loadInitializers(App, config.modulePrefix);
--- spa/app/router.js
+++ spa/app/router.js
@@ -2,10 +2,16 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
-Router.map(function() {
+Router.map(function () {
+    this.route('dashboard', { path: '/' });
+    this.route('posts');
+    this.route('tags');
+    this.route('category');
+    this.route('comments');
+    this.route('commentators');
 });
 
 export default Router;
