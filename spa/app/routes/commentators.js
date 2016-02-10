97dc6e846ce1d8b27eeb00ea0605e7f880b34592
morontt <morontt@gmail.com>
2016-02-11 01:09:00 +0200

    commentators

--- spa/app/routes/commentators.js
+++ spa/app/routes/commentators.js
@@ -6,6 +6,9 @@ export default Ember.Route.extend({
             refreshModel: true
         }
     },
+    model(params) {
+        return this.store.query('commentator', params);
+    },
     setupController(controller, model) {
         this._super(controller, model);
 
