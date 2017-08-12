33fc3d6b953a1bcae7c683ba4fcff3cb7d859c31
Alexander Harchenko <morontt@gmail.com>
2017-08-13 01:28:29 +0300

    install watchman

---
+++ var/docker/web/watchman-install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd /tmp
+
+git clone https://github.com/facebook/watchman.git
+cd watchman
+git checkout v4.7.0
+
+./autogen.sh
+./configure --without-python
+
+make
+make install
+
+cd ..
+rm -rf ./watchman
