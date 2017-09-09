354e01259e414a1fe6d041dc4151a90ac5528886
Alexander Harchenko <morontt@gmail.com>
2017-09-09 21:54:20 +0300

    require https

--- web/app_test.php
+++ web/app_test.php
@@ -10,11 +10,11 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
-        || php_sapi_name() === 'cli-server'
-        || getenv('APPLICATION_ENV') === 'development'
+if (getenv('APPLICATION_ENV') !== 'development'
+    && (isset($_SERVER['HTTP_CLIENT_IP'])
+        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+        || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+            || php_sapi_name() === 'cli-server')
 )) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
@@ -29,6 +29,7 @@ Debug::enable();
 $kernel = new AppKernel('test', true);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
+Request::setTrustedProxies(array('127.0.0.1', $request->server->get('REMOTE_ADDR')));
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
