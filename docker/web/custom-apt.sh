c7020d1a19f35b3189ee89bf36a9c78a170fb700
Alexander Harchenko <morontt@gmail.com>
2017-09-24 03:00:58 +0300

    small change in dockerfiles

--- docker/web/custom-apt.sh
+++ docker/web/custom-apt.sh
@@ -4,3 +4,7 @@ curl -sL https://deb.nodesource.com/setup_4.x | bash -
 
 apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
 echo "deb http://repo.mysql.com/apt/debian/ jessie mysql-5.7" > /etc/apt/sources.list.d/mysql.list
+
+apt-get update
+apt-get install -y --no-install-recommends nodejs mysql-client
+rm -rf /var/lib/apt/lists/*
