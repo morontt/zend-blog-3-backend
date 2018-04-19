15463fe8aa9271a644c66cfbab3f2c2c6d50a2f4
Alexander Harchenko <morontt@gmail.com>
2018-04-20 02:46:44 +0300

    upgrade Node.js

--- docker/web/custom-apt.sh
+++ docker/web/custom-apt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -sL https://deb.nodesource.com/setup_4.x | bash -
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
 
 apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
 echo "deb http://repo.mysql.com/apt/debian/ jessie mysql-5.7" > /etc/apt/sources.list.d/mysql.list
