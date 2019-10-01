2754dcb4f18e8f3ca5787211cadda74a265a4b68
Alexander Harchenko <morontt@gmail.com>
2019-10-01 19:18:27 +0300

    dockerfile fix (in progress, part 2)

--- docker/web/custom-apt.sh
+++ docker/web/custom-apt.sh
@@ -2,10 +2,8 @@
 
 apt-get update && apt-get install -y apt-utils gnupg2
 
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
-
 apt-get update && apt-get install -y --no-install-recommends \
-    zlib1g-dev libicu-dev git nano zip pngquant nodejs default-mysql-client libmagickwand-dev
+    zlib1g-dev libicu-dev git nano zip unzip pngquant nodejs npm default-mysql-client libmagickwand-dev
 
 apt-get clean
 
