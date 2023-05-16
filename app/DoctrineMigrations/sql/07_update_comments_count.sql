f471fe677762779857fb3ccb6fac49265face1d3
Alexander Harchenko <morontt@gmail.com>
2023-05-16 17:10:46 +0300

    update count query #61

--- app/DoctrineMigrations/sql/07_update_comments_count.sql
+++ app/DoctrineMigrations/sql/07_update_comments_count.sql
@@ -2,11 +2,16 @@ CREATE PROCEDURE update_comments_count(IN topicID INT UNSIGNED)
   BEGIN
     DECLARE count_comments INT DEFAULT 0;
 
-    SELECT COUNT(`id`)
-    INTO count_comments
-    FROM `comments` WHERE
-      (`post_id` = topicID)
-      AND NOT (deleted = 1 AND tree_right_key - tree_left_key = 1);
+    SELECT COUNT(DISTINCT c1.id) INTO count_comments
+    FROM
+      comments AS c1,
+      comments AS c2
+    WHERE
+      c1.post_id = topicID
+      AND c2.post_id = topicID
+      AND c1.tree_left_key <= c2.tree_left_key
+      AND c1.tree_right_key >= c2.tree_right_key
+      AND c2.deleted = 0;
 
     UPDATE `posts` SET `comments_count` = count_comments
     WHERE `posts`.`id` = topicID;
