87ce9679c7997c7d1c86507b02db06405d4216c6
Alexander Harchenko <morontt@gmail.com>
2023-05-24 08:56:19 +0300

    change files for production

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ rm -R ./web/bundles/*
 rm -R ./web/spa/*
 rm -R ./web/dist/*
 
-docker compose run --rm rhinoceros bash -c "./buildapp_php.sh"
+docker exec rhinoceros bash -c "./buildapp_php.sh"
 docker compose run --rm nodejs bash -c "./buildapp_js.sh -i"
-docker compose run --rm rhinoceros bash -c "chown -R www-data:www-data ."
-docker compose down
+docker exec rhinoceros bash -c "chown -R www-data:www-data ."
