7b246304a000f6edcbaeb8fce9578a4e5d5b36e7
Alexander Harchenko <morontt@gmail.com>
2023-05-27 21:50:53 +0300

    Save geolocation if exists

--- src/Mtt/BlogBundle/Service/CommentManager.php
+++ src/Mtt/BlogBundle/Service/CommentManager.php
@@ -7,6 +7,7 @@ use Mtt\BlogBundle\DTO\CommentDTO;
 use Mtt\BlogBundle\Entity\Comment;
 use Mtt\BlogBundle\Entity\Repository\CommentatorRepository;
 use Mtt\BlogBundle\Entity\Repository\CommentRepository;
+use Mtt\BlogBundle\Entity\Repository\GeoLocationRepository;
 use Mtt\BlogBundle\Entity\Repository\PostRepository;
 use Mtt\BlogBundle\Event\CommentEvent;
 use Mtt\BlogBundle\Exception\NotAllowedCommentException;
@@ -40,11 +41,17 @@ class CommentManager
      */
     private $postRepo;
 
+    /**
+     * @var GeoLocationRepository
+     */
+    private $geoRepo;
+
     public function __construct(
         Tracking $tracking,
         EventDispatcherInterface $dispatcher,
         CommentatorRepository $commentatorRepo,
         CommentRepository $commentRepo,
+        GeoLocationRepository $geoRepo,
         PostRepository $postRepo
     ) {
         $this->tracking = $tracking;
@@ -52,6 +59,7 @@ class CommentManager
         $this->commentatorRepo = $commentatorRepo;
         $this->commentRepo = $commentRepo;
         $this->postRepo = $postRepo;
+        $this->geoRepo = $geoRepo;
     }
 
     /**
@@ -88,6 +96,11 @@ class CommentManager
             if ($parent) {
                 $comment->setParent($parent);
             }
+        }
+
+        $location = $this->geoRepo->findOneByIpAddress($commentData->ipAddress);
+        if ($location) {
+            $comment->setGeoLocation($location);
         }
 
         $commentator = $this->commentatorRepo->findOrCreate($commentData->commentator);
