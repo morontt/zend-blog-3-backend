7e5cfb30167fb769e020ae24203c9da78456a97f
Alexander Harchenko <morontt@gmail.com>
2023-06-03 18:59:03 +0300

    Fix gravatar in list of comments #81

--- spa/app/models/comment.js
+++ spa/app/models/comment.js
@@ -23,11 +23,11 @@ export default DS.Model.extend({
             url = `${app_parameters.cdn_url}/images/avatar/${this.get('imageHash')}.png`;
         } else {
             let defaults = ['wavatar', 'monsterid'];
-            let idx = (this.get('id')) % 2;
+            let idx = (this.get('commentatorId')) % 2;
 
             url = `//www.gravatar.com/avatar/${this.get('emailHash')}?d=${defaults[idx]}`;
         }
 
         return url;
-    }.property('id', 'emailHash', 'imageHash')
+    }.property('commentatorId', 'emailHash', 'imageHash')
 });
