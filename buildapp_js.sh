b862c17249c4759fd946ca0737503939f6e1dca4
Alexander Harchenko <morontt@gmail.com>
2023-06-03 22:27:31 +0300

    assets improvement

--- buildapp_js.sh
+++ buildapp_js.sh
@@ -2,28 +2,38 @@
 
 # Run inside nodejs container
 
-bower install --allow-root
-cp -R bower_components/bootstrap/fonts web
-cp -R bower_components/jquery-ui/themes/base/images web/dist
-yarn install
-
-cd ./spa
+WITH_INSTALL=""
 
 while getopts ":ri" opt; do
   case $opt in
     r)
       rm -rf node_modules
       rm -rf bower_components
+      rm -rf spa/node_modules
+      rm -rf spa/bower_components
       ;;
     i)
-      bower install --allow-root
-      yarn install
+      WITH_INSTALL="yes"
       ;;
     *)
       echo "unknown flag :("
       exit 1
   esac
 done
+
+if [[ "$WITH_INSTALL" == "yes" ]]; then
+  bower install
+  cp -R bower_components/bootstrap/fonts web
+  cp -R bower_components/jquery-ui/themes/base/images web/dist
+  yarn install
+fi
+
+cd ./spa || exit
+
+if [[ "$WITH_INSTALL" == "yes" ]]; then
+  bower install
+  yarn install
+fi
 
 ember build --output-path ./../web/spa
 
