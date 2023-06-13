f0af92d10d798d4c44b25b727c58e4b977a8000e
Alexander Harchenko <morontt@gmail.com>
2023-06-13 17:31:37 +0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 rm -R ./var/cache/*
 rm -R ./web/bundles/*
 rm -R ./web/spa/*
-rm -R ./web/dist/*
 
 docker exec rhinoceros bash -c "./buildapp_php.sh"
 docker compose run --rm nodejs bash -c "./buildapp_js.sh -i"
