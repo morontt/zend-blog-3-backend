932a2cd79b4461e6fb8f4e8204c66df5ebd759df
Alexander Harchenko <morontt@gmail.com>
2023-06-20 22:09:30 +0300

    Add /start command to Telegram bot

---
+++ src/Mtt/BlogBundle/Telegram/Command/Start.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Mtt\BlogBundle\Telegram\Command;
+
+use Xelbot\Telegram\Command\TelegramCommandInterface;
+use Xelbot\Telegram\Command\TelegramCommandTrait;
+use Xelbot\Telegram\Entity\Message;
+use Xelbot\Telegram\Robot;
+
+class Start implements TelegramCommandInterface
+{
+    use TelegramCommandTrait;
+
+    /**
+     * @param Message $message
+     */
+    public function execute(Message $message): void
+    {
+        $text = 'Приветствую тебя, человек ' . Robot::EMOJI_ROBOT;
+        $text .= "\n\nЯ служу своему создателю и выполняю некоторые получения по его блогу.";
+        $text .= ' Не знаю, чем могу быть тебе полезен, но можешь писать сюда что угодно.';
+        $text .= ' Возможно, получишь какой-нибудь ответ, поскольку пишешь не совсем в космос ';
+        $text .= '&#x2728;';
+
+        $this->requester->sendMessage([
+            'chat_id' => $message->getChat()->getId(),
+            'text' => $text,
+            'parse_mode' => 'HTML',
+        ]);
+    }
+}
