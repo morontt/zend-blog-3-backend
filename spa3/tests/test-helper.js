1c37f55e4cc611bcaf5dc4ef82faf4fda2bf1256
Tomster <tomster@emberjs.com>
2023-07-01 16:30:11 +0300

    Initial Commit from Ember CLI v3.28.6
                                                            _...,
                                                         ,:^;,...;
                  -+===;.         ,,--++====++-,,,     .:  /.....,
                :::::~+++++#:,+#++++++++++++++++++#*..:   /,......
               (,,,,,,::=+++##++++++++++++++++++++++#.   :....../
                ...,,,,,::++++++++++++++++++++++++++++++*..,...:
                *..+...,#@@@@@@@@@++++++++++++++++++++++#*....*
                  @#,;##############@@@+*+#@@@@@@@@@@#*++#..<
                  *@##@@+,-*^^^*-+@####@@@######@@@#####@@,,,+
                    @#@*            @#@@@@#@@+--*^^*--#@@@@@@#
                    @#@.    @#      @##+++@#,           .@@#@@
                     \#@#    @@     +@@++++#@@     @@     :@@
                     :@#*         @#@++++++@#*    #@     @@+
                    :*+@@#;,.__.+@#@+,-^^.++@#          @@++
                   ;*  :*@@@##@@@@;++r._j^.+@##@+,.__,,@@++.
                  /*    ........+++++++++++++#@@@@@###@@#++++,
                ,:       ...,@@@#++===----==@@@####,,....+++++
               .:       ......@@##@\   ;   :@####@,,...... +++.
               ;       .........@###,   ;  ;xx#@;,,.....   *;+,
               |       ........,*;xxxx--^--=xxx,........   :+#;
               ;         ......,,;xxxxxxxxxxxxx;,.....     *+#
                ;          ......,::xxxx;.     ......       +.   .
                 *;            .........  +###  ....       / ,. /:| ,.
                   .+:             ...  ;##++##, .      ,#. (..v..;*./
                      **                ##  ###*    .:*&&&+. \.,....<,
                       \#&+**==-..,,__  ;##  ###  :,*+&&&&&&&v+#&,,.._/
                      \#&&&&*...,::,,.  ##; ,##* .*****;:&&&&&&&&&
                     ,+*+;~*..*** *.* ### ###* *******    *+#&;*
                                      \##,;##    ****    :,  **
             \#####    ##   ###  ###,  ########       .#####   ;##  ##
            \#######  ;##  #### ,###.  ##########    ########  ### ####
           \###  ###  ### ##########   ####  ####   ,##   ###  #######*
          \### ,###  ##############:   ##     ###  #### ,##   :#### ###  ##;
      \##########    ########### ##   .##    ,###  #######    ##### :######
        \######    .###### ####  ##   ###    ### ######*    :#####   ####
           \#############  ####  ################    ######## ###
            \#####*  *#*    #:   :###   *###*         *####    #*

---
+++ spa3/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'mtt-blog/app';
+import config from 'mtt-blog/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
