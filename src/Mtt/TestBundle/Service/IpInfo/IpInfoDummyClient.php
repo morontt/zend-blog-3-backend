4a42a5e8ac00d3c2df9045f84d3819f7605dbf71
Alexander Harchenko <morontt@gmail.com>
2023-07-01 12:32:28 +0300

    fake data for IP-geolocation #79

--- src/Mtt/TestBundle/Service/IpInfo/IpInfoDummyClient.php
+++ src/Mtt/TestBundle/Service/IpInfo/IpInfoDummyClient.php
@@ -2,8 +2,10 @@
 
 namespace Mtt\TestBundle\Service\IpInfo;
 
+use Faker\Factory as FakerFactory;
 use Mtt\BlogBundle\Service\IpInfo\IpInfoClientInterface;
 use Mtt\BlogBundle\Service\IpInfo\LocationInfo;
+use Symfony\Component\Intl\Intl;
 
 class IpInfoDummyClient implements IpInfoClientInterface
 {
@@ -14,11 +16,31 @@ class IpInfoDummyClient implements IpInfoClientInterface
      */
     public function getLocationInfo(string $ip): ?LocationInfo
     {
+        if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+            return null;
+        }
+
+        $faker = FakerFactory::create();
+        $faker->seed(ip2long($ip));
+
+        $countries = Intl::getRegionBundle()->getCountryNames('en_US');
+        do {
+            $countryCode = null;
+            $country = $faker->country;
+            foreach ($countries as $key => $value) {
+                if ($country == $value) {
+                    $countryCode = $key;
+                    break;
+                }
+            }
+        } while (is_null($countryCode));
+
         return LocationInfo::createFromArray([
-            'countryCode' => 'SU',
-            'countryName' => 'Soviet Union',
-            'regionName' => 'Lukomorie',
-            'cityName' => 'Myxosransk',
+            'countryCode' => $countryCode,
+            'countryName' => $country,
+            'regionName' => $faker->state,
+            'cityName' => $faker->city,
+            'zipCode' => $faker->postcode,
         ]);
     }
 }
