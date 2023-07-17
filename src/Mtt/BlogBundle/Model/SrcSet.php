d031643bc213d12216ce22d2d4d589da1d20b33a
Alexander Harchenko <morontt@gmail.com>
2023-07-17 20:08:28 +0300

    check size alternative image

--- src/Mtt/BlogBundle/Model/SrcSet.php
+++ src/Mtt/BlogBundle/Model/SrcSet.php
@@ -2,6 +2,8 @@
 
 namespace Mtt\BlogBundle\Model;
 
+use Mtt\BlogBundle\Service\ImageManager;
+
 class SrcSet
 {
     private ?SrcSetItem $origin;
@@ -16,13 +18,13 @@ class SrcSet
     }
 
     /**
-     * @param array $origin
+     * @param array $items
      *
      * @return SrcSet
      */
-    public function setOrigin(array $origin): SrcSet
+    public function setOrigin(array $items): SrcSet
     {
-        $this->origin = new SrcSetItem($origin);
+        $this->origin = new SrcSetItem($items);
 
         return $this;
     }
@@ -36,14 +38,38 @@ class SrcSet
     }
 
     /**
-     * @param array $webp
+     * @param array $items
      *
      * @return SrcSet
      */
-    public function setWebp(array $webp): SrcSet
+    public function setWebp(array $items): SrcSet
     {
-        $this->webp = new SrcSetItem($webp);
+        $this->webp = new SrcSetItem($items);
+        if ($this->origin && !$this->isSizeSmaller($items)) {
+            $this->webp = null;
+        }
 
         return $this;
+    }
+
+    public function isSizeSmaller(array $items)
+    {
+        if (!$this->origin) {
+            return true;
+        }
+
+        $firstItemSize = 0;
+        foreach ($items as $item) {
+            $firstItemSize = filesize(ImageManager::getUploadsDir() . '/' . $item['path']);
+            break;
+        }
+
+        $originItemSize = 0;
+        foreach ($this->origin->getItems() as $item) {
+            $originItemSize = filesize(ImageManager::getUploadsDir() . '/' . $item['path']);
+            break;
+        }
+
+        return $firstItemSize < $originItemSize;
     }
 }
