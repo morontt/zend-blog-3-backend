a7845a31a2e25cbb1eb2a5a5a81c501243b17fcd
Alexander Kharchenko <morontt@yandex.ru>
2024-05-27 16:11:24 +0300

    disable check token

--- src/Mtt/BlogBundle/Controller/GitRepoController.php
+++ src/Mtt/BlogBundle/Controller/GitRepoController.php
@@ -27,13 +27,13 @@ class GitRepoController extends AbstractController
     {
         $logfile = fopen(APP_VAR_DIR . '/logs/gitflic.log', 'a');
 
-        $token = $request->headers->get('gitflic-webhook-secret');
+        /* $token = $request->headers->get('gitflic-webhook-secret');
         if (!hash_equals(sha1($this->secretToken), sha1($token))) {
             fwrite($logfile, date('Y-m-d H:i:s') . ": 403 Forbidden\n");
             fclose($logfile);
 
             return new Response("Get out!\n", 403);
-        }
+        } */
 
         fwrite($logfile, date('Y-m-d H:i:s') . ': ');
         fwrite($logfile, print_r($request->headers->all(), true));
