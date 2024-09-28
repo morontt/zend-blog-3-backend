62adea3b4f09279e2509c80743d38fc91b7f17d0
Alexander Kharchenko <morontt@yandex.ru>
2024-09-28 11:18:32 +0300

    prepare to migration

---
+++ migrations/sql/doctrine_migrations.sql
@@ -0,0 +1,80 @@
+create table doctrine_migrations
+(
+    version        varchar(191) not null
+        primary key,
+    executed_at    datetime     null,
+    execution_time int          null
+)
+    collate = utf8mb3_unicode_ci;
+
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20141025170346', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20141026012219', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20150223224004', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20150614171030', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20150620132229', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20150721233323', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20160331221426', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20160402200756', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20160409151334', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20160410165901', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20160425235303', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20160511221425', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20161113144623', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20161119084454', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20170527224637', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20170724193800', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20170806102146', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20170826150356', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20170831214559', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20171007084615', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20171113212500', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20200410220309', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20200411070036', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20200418080413', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20211219205322', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20220106212828', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20220130185648', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20220202064937', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20220319113011', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20220321171948', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230516083348', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230518121106', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230521160100', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230524141234', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230530121844', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230603111902', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230603113418', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230603121221', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230609090837', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230609101954', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230611071550', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230613040158', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230613134523', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230614083419', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230614091935', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230614100944', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230616135557', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230616153217', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230616155430', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230617063134', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230617072331', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230617074543', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230620044927', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230620154440', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230621071308', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230621174221', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230626144738', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230626214411', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230710071824', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230711093426', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20230714065712', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20240608123228', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20240720141433', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20240720141951', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20240808221737', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20240809184435', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20240811070520', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20240825082206', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20240830055032', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20240907072911', null, null);
+INSERT INTO morontt_db.doctrine_migrations (version, executed_at, execution_time) VALUES ('Application\\Migrations\\Version20240918062620', '2024-09-25 19:37:58', 25);
