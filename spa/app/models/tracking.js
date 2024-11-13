a03922e6465226742fafc749c314da6ce8c36f88
Alexander Kharchenko <morontt@yandex.ru>
2024-11-13 10:08:34 +0300

    Fix private IP
    ip2long from https://github.com/wangwenming/ip2long

--- spa/app/models/tracking.js
+++ spa/app/models/tracking.js
@@ -1,4 +1,18 @@
 import DS from 'ember-data';
+import net from 'mtt-blog/utils/ip2long';
+
+/**
+ * Following RFC 1918, Section 3
+ *
+ * 10.0.0.0    - 10.255.255.255  (10/8 prefix)
+ * 172.16.0.0  - 172.31.255.255  (172.16/12 prefix)
+ * 192.168.0.0 - 192.168.255.255 (192.168/16 prefix)
+ */
+const cidrs = [
+    {ip: '10.0.0.0', mask: 8},
+    {ip: '172.16.0.0', mask: 12},
+    {ip: '192.168.0.0', mask: 16},
+];
 
 export default DS.Model.extend({
     statusCode: DS.attr('number'),
@@ -16,6 +30,11 @@ export default DS.Model.extend({
     method: DS.attr('string'),
     createdAt: DS.attr('date'),
     privateIP: function () {
-        return this.get('city') === '-';
-    }.property('city'),
+        let ipLong = net.ip2long(this.get('ipAddr'));
+        let isPrivate = false;
+
+        return cidrs.reduce(function (accumulator, current) {
+            return accumulator || (ipLong >> (32 - current.mask)) === (net.ip2long(current.ip) >> (32 - current.mask));
+        }, isPrivate);
+    }.property('ipAddr'),
 });
