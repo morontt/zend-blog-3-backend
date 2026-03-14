27e6adb0950f41b2c6e8441f6cc61e785cd4c544
Alexander Kharchenko <morontt@yandex.ru>
2026-03-14 10:21:36 +0300

    Change test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-docker exec hedgehog_mysql /bin/bash -c "echo \"
-    DROP DATABASE IF EXISTS mttblog_test;
-    CREATE DATABASE mttblog_test;
-    GRANT ALL PRIVILEGES ON mttblog_test. * TO 'mttblog'
-\" | mysql -uroot -pdocker"
+# docker exec hedgehog_mysql /bin/bash -c "echo \"
+#     DROP DATABASE IF EXISTS mttblog_test;
+#     CREATE DATABASE mttblog_test;
+#     GRANT ALL PRIVILEGES ON mttblog_test. * TO 'mttblog'
+# \" | mysql -uroot -pdocker"
 
-docker-compose exec rhinoceros bash -c "bin/console doctrine:migrations:migrate -e test --no-interaction \\
-    && bin/console doctrine:fixtures:load -e test --no-interaction"
-docker-compose exec rhinoceros bash -c "bin/php-cs-fixer fix --dry-run --diff \\
-    && chown -R www-data:www-data var \\
-    && bin/behat @MttTestBundle \\
-    && bin/phpspec run"
+# docker-compose exec rhinoceros bash -c "bin/console doctrine:migrations:migrate -e test --no-interaction \\
+#     && bin/console doctrine:fixtures:load -e test --no-interaction"
+# docker-compose exec rhinoceros bash -c "bin/php-cs-fixer fix --dry-run --diff \\
+#     && chown -R www-data:www-data var \\
+#     && bin/behat @MttTestBundle \\
+#     && bin/phpspec run"
+
+./var/git_hook/pre-push
