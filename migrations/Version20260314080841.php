0735d09b60af0e2b22b04c3c196c6272bc2fb399
Alexander Kharchenko <morontt@yandex.ru>
2026-03-14 12:04:27 +0300

    Replace body element in emails

---
+++ migrations/Version20260314080841.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260314080841 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE subscription_settings CHANGE subs_type subs_type SMALLINT DEFAULT 1 NOT NULL COMMENT \'0: none, 1: reply\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE subscription_settings CHANGE subs_type subs_type SMALLINT DEFAULT 1 NOT NULL COMMENT \'1: reply\'');
+    }
+}
