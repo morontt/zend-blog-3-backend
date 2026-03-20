dcec54fb177765fd71a980c0c9671297f0e67a30
Alexander Kharchenko <morontt@yandex.ru>
2026-03-20 19:40:08 +0300

    Upgrade vendors for Symfony 6.0

--- src/App/Validator/Constraints/DateTimeStringValidator.php
+++ src/App/Validator/Constraints/DateTimeStringValidator.php
@@ -17,7 +17,7 @@ use Symfony\Component\Validator\Exception\UnexpectedValueException;
 
 class DateTimeStringValidator extends ConstraintValidator
 {
-    public function validate($value, Constraint $constraint): void
+    public function validate(mixed $value, Constraint $constraint): void
     {
         if (!$constraint instanceof DateTimeString) {
             throw new UnexpectedTypeException($constraint, DateTimeString::class);
