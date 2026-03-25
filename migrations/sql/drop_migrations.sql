164175654690e32049360c2f1957407860e5f223
Alexander Kharchenko <morontt@yandex.ru>
2026-03-25 09:51:16 +0300

    Remove "tracking_to_archive" procedure

--- migrations/sql/drop_migrations.sql
+++ migrations/sql/drop_migrations.sql
@@ -1,7 +1,6 @@
 -- Drop migrations with procedures
 
 DELETE FROM doctrine_migrations
-  WHERE version = 'Application\\Migrations\\Version20240720141951' -- tracking_to_archive
-    OR version = 'Application\\Migrations\\Version20171113212500'  -- update_all_comments_count
+  WHERE version = 'Application\\Migrations\\Version20171113212500'  -- update_all_comments_count
     OR version = 'Application\\Migrations\\Version20230516083348'  -- update_comments_count
 ;
