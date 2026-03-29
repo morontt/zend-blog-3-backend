34f535c98d6594ad6ff024513170fd53386b2e0b
Alexander Kharchenko <morontt@yandex.ru>
2026-03-29 15:01:26 +0300

    Handle error executing command

--- src/App/Service/TaskService.php
+++ src/App/Service/TaskService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Exception\AppException;
 use App\Exception\ObjectNotFoundException;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
@@ -23,9 +24,10 @@ class TaskService
     /**
      * @param array<string, mixed> $params
      *
+     * @throws AppException
      * @throws ObjectNotFoundException
      */
-    public function runCommand(string $commandClassName, array $params = []): int
+    public function runCommand(string $commandClassName, array $params = []): void
     {
         $this->logger->info('Run command', [
             'command' => $commandClassName,
@@ -37,7 +39,16 @@ class TaskService
         $input = new ArrayInput($params);
         $input->setInteractive(false);
 
-        return $command->run($input, new NullOutput());
+        $exitCode = $command->run($input, new NullOutput());
+        if ($exitCode !== Command::SUCCESS) {
+            $this->logger->info('Error executing command', [
+                'command' => $commandClassName,
+                'params' => $params,
+                'exit_code' => $exitCode,
+            ]);
+
+            throw new AppException("Error executing {$commandClassName} command");
+        }
     }
 
     public function add(Command $command): void
