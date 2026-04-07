3cc217baee3939f7eede8fe444edebb0f3e64504
Alexander Kharchenko <morontt@yandex.ru>
2026-04-07 17:29:44 +0300

    Add helper function

--- src/App/Utils/functions.php
+++ src/App/Utils/functions.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 /**
  * User: morontt
- * Date: 07.03.2026
+ * Date: 07.04.2026
  * Time: 11:27
  */
 
 namespace App\Utils;
 
-// @phpstan-ignore return.unusedType
 function base64url_decode(string $value): string|false
 {
-    return base64_decode(str_replace(['-', '_'], ['+', '/'], $value));
+    return base64_decode(str_replace(['-', '_'], ['+', '/'], $value), true);
+}
+
+function base64url_encode(string $value): string
+{
+    return str_replace(['+', '/'], ['-', '_'], base64_encode($value));
 }
